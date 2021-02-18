71faf1fd7f1b17420e9082187aa394349f846251
dvalinn <tiago.andre.amorim@gmail.com>
2021-02-18 19:20:22 +0000

    corrected the path of arucoDictTranslator on colAruco.py

--- pythonInputHandler/colAruco.py
+++ pythonInputHandler/colAruco.py
@@ -48,7 +48,7 @@ def arduino_read():
 def fetch_aruco(id: str, size: str) -> str:
 
     completed_process = subprocess.run(
-        ["/home/dvalinn/prog/arucoOpenCV/colAruco_code_gen/arucoDictTranslator", id, size], capture_output=True, text=True)
+        ["../colAruco_code_gen/arucoDictTranslator", id, size], capture_output=True, text=True)
     return completed_process.stdout.strip("\n").strip()
 
 
