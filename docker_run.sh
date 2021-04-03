e21b3c668f495ddd9edf57ae0194c870fdcb0aa2
dvalinn <tiago.andre.amorim@gmail.com>
2021-04-03 19:15:16 +0100

    removed files from develop branch that shouldnt have passed to master

--- docker_run.sh
+++
@@ -1,2 +0,0 @@
-# docker run --device=/dev/video1 --device=/dev/video2 -it ros_melodic_docker:latest
-docker run --device=/dev/video1 -it ros_melodic_docker:latest
--- setup_workspace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /catkin_ws/src/
-cd /catkin_ws
-
-. /opt/ros/melodic/setup.bash
-catkin_make
-
-echo "source . /opt/ros/melodic/setup.bash" >> /root/.bashrc
