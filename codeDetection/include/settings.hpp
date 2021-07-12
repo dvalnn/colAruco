dc28c0e8b63261a7dce90ac48cc1dee8a8e3b94a
dvalinn <tiago.andre.amorim@gmail.com>
2021-07-12 12:23:28 +0100

    added camera calibration script from openCv gh page

---
+++ codeDetection/include/settings.hpp
@@ -0,0 +1,14 @@
+class CameraSettings {
+   public:
+};
+
+class ArucoSettings {
+   public:
+};
+class Settings {
+   public:
+    bool cameraOk = true;
+    bool arucoOK = true;
+
+   Settings(){}
+};
