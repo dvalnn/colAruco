fac9188c1a95c3da6c057cde4a357249a9fd02bf
dvalinn <tiago.andre.amorim@gmail.com>
2021-08-15 13:04:28 +0100

    integrated cameraCalibration functionality into cameraSettings class

--- codeDetection/include/cameraSettings.hpp
+++ codeDetection/include/cameraSettings.hpp
@@ -1,13 +1,21 @@
-#include <map>
-#include <string>
-
+#include <opencv2/core.hpp>
 #include <opencv2/aruco.hpp>
+
+using namespace std;
+
 class CameraSettings {
    public:
     bool OK = false;
     cv::Mat cameraMatrix;
     cv::Mat distortionCoeffs;
 
-    CameraSettings(std::string filepath);
-    void createFile(std::string filepath, cv::Mat camMatrix, cv::Mat distCoeffs);
+    CameraSettings(string filepath);
+    bool runCalibrationAndSave();
+
+   private:
+    bool saveCalibrationResults(string filepath, cv::Mat camMatrix, cv::Mat distCoeffs);
+    void createKnownBoardPositions(cv::Size boardSize, float squareEdgelength, vector<cv::Point3f>& corners);
+    void getChessboardCorners(vector<cv::Mat> images, vector<vector<cv::Point2f>>& allFoundCorners, bool showResults);
+    void cameraCalibration(vector<cv::Mat> calibrationImages, cv::Size boardSize, float squareEdgeLength,
+                           cv::Mat& cameraMatrix, cv::Mat& distanceCoefficients);
 };
--- codeDetection/src/cameraSettings.cpp
+++ codeDetection/src/cameraSettings.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/calib3d.hpp>
+
 #include "../include/cameraSettings.hpp"
+
+const cv::Size chessboardSize = cv::Size(7, 12);
+const float calibrationSquareSize = 0.020f;  //meters
+const float arucoMarkerSize = 0.0905f;       //meters
 
 using namespace std;
 
@@ -10,22 +20,11 @@ bool filenameIsValid(const string filename) {
     return filename.ends_with(".yml") or filename.ends_with(".xml") or filename.ends_with(".json");
 }
 
-void CameraSettings::createFile(string filepath, cv::Mat camMatrix, cv::Mat distCoeffs) {
-    if (not filenameIsValid(filepath)) {
-        cout << INVALID_PATH_ERROR_MSG << endl;
-        return;
-    }
-
-    cv::FileStorage fs(filepath, cv::FileStorage::WRITE);
-    if (not fs.isOpened()) {
-        cout << "[ERROR] could not open specified file (" << filepath << ") " << endl;
-        return;
-    }
-
-    fs << "Camera_Matrix" << camMatrix;
-    fs << "Distortion_Coefficients" << distCoeffs;
-}
-
+/**
+ * @brief Import cameraSettings from previously existing file
+ * 
+ * @param filepath 
+ */
 CameraSettings::CameraSettings(string filepath) {
     if (not filenameIsValid(filepath)) {
         cout << INVALID_PATH_ERROR_MSG << endl;
@@ -47,9 +46,148 @@ CameraSettings::CameraSettings(string filepath) {
 
     if (cameraMatrix.empty() or distortionCoeffs.empty()) {
         cout << "[ERROR] Invalid data (" << filepath << ")" << endl;
-        OK = false;
         return;
     }
 
     OK = true;
+}
+
+bool CameraSettings::saveCalibrationResults(string filepath, cv::Mat camMatrix, cv::Mat distCoeffs) {
+    if (not filenameIsValid(filepath)) {
+        cout << INVALID_PATH_ERROR_MSG << endl;
+        return false;
+    }
+
+    cv::FileStorage fs(filepath, cv::FileStorage::WRITE);
+    if (not fs.isOpened()) {
+        cout << "[ERROR] could not open specified file (" << filepath << ") " << endl;
+        return false;
+    }
+
+    fs << "Camera_Matrix" << camMatrix;
+    fs << "Distortion_Coefficients" << distCoeffs;
+
+    return true;
+}
+
+void CameraSettings::createKnownBoardPositions(cv::Size boardSize, float squareEdgelength,
+                                               vector<cv::Point3f>& corners) {
+    for (int i = 0; i < boardSize.height; i++) {
+        for (int j = 0; j < boardSize.width; j++) {
+            corners.push_back(cv::Point3f(j * squareEdgelength, i * squareEdgelength, 0.0f));  // z value is always 0
+        }
+    }
+}
+
+/**
+ * @brief Get all the chessboard corners from a list of images
+ * 
+ * @param images 
+ * @param allFoundCorners 
+ * @param showResults 
+ */
+void CameraSettings::getChessboardCorners(vector<cv::Mat> images, vector<vector<cv::Point2f>>& allFoundCorners,
+                                          bool showResults) {
+    for (vector<cv::Mat>::iterator iter = images.begin(); iter != images.end(); iter++) {
+        vector<cv::Point2f> pointBuffer;
+        auto detectionFlags = cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE;
+        bool found = cv::findChessboardCorners(*iter, chessboardSize, pointBuffer, detectionFlags);
+
+        if (found)
+            allFoundCorners.push_back(pointBuffer);
+
+        if (showResults) {
+            cv::drawChessboardCorners(*iter, chessboardSize, pointBuffer, found);
+            cv::imshow("looking for corners", *iter);
+            cv::waitKey(0);
+        }
+    }
+}
+
+void CameraSettings::cameraCalibration(vector<cv::Mat> calibrationImages, cv::Size boardSize, float squareEdgeLength,
+                                       cv::Mat& cameraMatrix, cv::Mat& distanceCoefficients) {
+    vector<vector<cv::Point2f>> chessboardImageSpacePoints;
+    getChessboardCorners(calibrationImages, chessboardImageSpacePoints, false);
+
+    vector<vector<cv::Point3f>> worldSpaceCornerPoints(1);
+
+    createKnownBoardPositions(boardSize, squareEdgeLength, worldSpaceCornerPoints[0]);
+    worldSpaceCornerPoints.resize(chessboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+
+    vector<cv::Mat> rVectors, tVectors;
+    distanceCoefficients = cv::Mat::zeros(8, 1, CV_64F);
+
+    cv::calibrateCamera(worldSpaceCornerPoints, chessboardImageSpacePoints, boardSize,
+                        cameraMatrix, distanceCoefficients, rVectors, tVectors);
+}
+
+bool CameraSettings::runCalibrationAndSave() {
+    cv::Mat frame;
+
+    cv::Mat cameraMatrix = cv::Mat::eye(3, 3, CV_64F);
+    cv::Mat distanceCoefficients;
+
+    vector<cv::Mat> savedImages;
+    vector<vector<cv::Point2f>> markerCorners, rejectedCandidates;
+
+    cv::VideoCapture vid(1);
+
+    if (!vid.isOpened())
+        return 0;
+
+    int framesPerSecond = 30;
+    int nImages = 0;
+    
+    cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
+
+    while (true) {
+        if (!vid.read(frame))
+            break;
+
+        vector<cv::Vec2f> foundPoints;
+        auto detectionFlags = cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE;
+        bool found = cv::findChessboardCorners(frame, chessboardSize, foundPoints, detectionFlags);
+
+        cv::Mat drawToFrame;
+        frame.copyTo(drawToFrame);
+        cv::drawChessboardCorners(drawToFrame, chessboardSize, foundPoints, found);
+
+        if (found)
+            cv::imshow("Webcam", drawToFrame);
+        else
+            cv::imshow("Webcam", frame);
+
+        char character = cv::waitKey(1000 / framesPerSecond);
+
+        switch (character) {
+            case 13:  // ENTER
+                //starting calibration
+                if (savedImages.size() >= 30) {
+                    string filename = "../resources/calib_results.json";
+
+                    cameraCalibration(savedImages, chessboardSize, calibrationSquareSize, cameraMatrix, distanceCoefficients);
+                    cout << "Calibration successful\nSaving results to " << filename << endl;
+
+                    saveCalibrationResults(filename, cameraMatrix, distanceCoefficients);
+                    cout << "Press any key to end calibration" << endl;
+
+                    cv::waitKey(0);
+                    return true;
+                }
+
+            case 27:  // ESC
+                //exiting program
+                return 0;
+
+            case 32:  // SPACE
+                //saving image
+                cv::Mat temp;
+                frame.copyTo(temp);
+                savedImages.push_back(temp);
+                cout << "image saved (" << ++nImages << "/30)\n";
+                break;
+        }
+    }
+
+    return 0;
 }
